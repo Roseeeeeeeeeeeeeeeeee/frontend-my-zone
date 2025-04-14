2f4ec52b5cfe4cc25d80125d66c20dd2d32583db
Roseeeeeeeeeeeeeeeeee <165487175+Roseeeeeeeeeeeeeeeeee@users.noreply.github.com>
2025-04-14 23:54:01 +0800

    开发头像组件

--- my-site/src/App.vue
+++ my-site/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
-   
+   <Avatar url="https://img0.baidu.com/it/u=218761232,3524899133&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=970" :size="100"/>
+  
   </div>
 </template>
 
 <script>
-
+import Avatar from './components/Avatar'
 
 export default {
   name: 'App',
- 
+  components:{
+    Avatar,
+  }
 }
 </script>
 
--- my-site/src/components/Avatar.vue
+++ my-site/src/components/Avatar.vue
@@ -1,11 +1,33 @@
 <template>
-    
+    <div>
+        <img class="avatar-img" :src="url" :style="{
+            width: size + 'px',
+            height: size + 'px'
+        }" />
+
+    </div>
+
 </template>
 
 <script>
 export default {
-
+    props: {
+        url: {
+            type: String,
+            required: true
+        },
+        size: {
+            type: Number,
+            required: true
+        }
+    }
 }
 </script>
 
-<style></style>
+<style scoped>
+.avatar-img {
+    border-radius: 50% ;
+    object-fit : cover;
+    display :block
+}
+</style>
