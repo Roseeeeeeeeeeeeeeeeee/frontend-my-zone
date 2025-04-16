c4bedfee285d6afc3ad94ee9df0f1242299a583d
Roseeeeeeeeeeeeeeeeee <165487175+Roseeeeeeeeeeeeeeeeee@users.noreply.github.com>
2025-04-16 20:08:20 +0800

    封装图标组件

--- my-site/src/App.vue
+++ my-site/src/App.vue
@@ -1,22 +1,25 @@
 <template>
   <div id="app">
-   <Avatar url="https://img0.baidu.com/it/u=218761232,3524899133&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=970" :size="100"/>
-   <Avatar url="https://img0.baidu.com/it/u=218761232,3524899133&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=970" />
-  
+    <Icon type="home"/>
+<h1>dhsjfka</h1>
   </div>
 </template>
 
 <script>
-import Avatar from './components/Avatar'
+import Icon from './components/Icon.vue';
 
 export default {
   name: 'App',
   components:{
-    Avatar,
+    Icon,
   }
 }
 </script>
 
-<style lang="less">
-
+<style scoped>
+.iconfont{
+  font-size: 400px
+}
 </style>
+
+
