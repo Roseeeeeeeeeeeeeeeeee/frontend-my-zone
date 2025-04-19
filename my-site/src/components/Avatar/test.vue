1483c83fad1c9295cd98d5fd0c2ae61e14342222
Roseeeeeeeeeeeeeeeeee <165487175+Roseeeeeeeeeeeeeeeeee@users.noreply.github.com>
2025-04-19 20:07:58 +0800

    规范测试组件格式

--- my-site/src/components/Avatar/test.vue
+++ my-site/src/components/Avatar/test.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="test-container">
      <Avatar url="https://img1.baidu.com/it/u=1586447300,143704852&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=767"/>
     </div>
   </template>
--- my-site/src/components/Pager/test.vue
+++ my-site/src/components/Pager/test.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="test-container">
     <Pager 
         :total="total"
         :current="current"
