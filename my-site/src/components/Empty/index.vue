25f6cac7fd95d54b89c3ced18334752a18266d44
Roseeeeeeeeeeeeeeeeee <165487175+Roseeeeeeeeeeeeeeeeee@users.noreply.github.com>
2025-04-19 20:34:46 +0800

    开发Empty组件

---
+++ my-site/src/components/Empty/index.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <div class="empty-container">
+        <Icon type="empty" />
+        <p class="desc">{{ text }}</p>
+    </div>
+
+</template>
+
+<script>
+import Icon from '../Icon';
+
+export default {
+    components: {
+        Icon,
+    },
+    props: {
+        text: {
+            type: String,
+            // required:false,//默认就是false所以可以不写 
+            default: '无数据'
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+@import '~@/styles/var.less';
+@import '~@/styles/mixin.less';
+
+.empty-container{
+    //自定义的混入（实现了自居中）
+   .self-center() ;
+    color: @gray;
+    text-align: center;
+}
+.icon-container{
+    font-size: 70px;
+}
+
+</style>
---
+++ my-site/src/components/Empty/test.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="test-container">
+     <Empty/>
+    </div>
+  </template>
+  
+  <script>
+import Empty from './';
+  export default {
+    components: {
+      Empty,
+    },
+   
+  }
+  </script>
+  
+  <style scoped>
+  .test-container{
+    width: 400px;
+    height: 600px;
+    border: 2px solid black;
+    /* 保证实现其内部组件自居中时的position:absolute有效 */
+    position: relative;
+  }
+  </style>
+  
