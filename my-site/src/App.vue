be7e1d1687a97c28adc41b2b9c36f4649c638ff2
Roseeeeeeeeeeeeeeeeee <165487175+Roseeeeeeeeeeeeeeeeee@users.noreply.github.com>
2025-04-19 20:08:40 +0800

    --

--- my-site/src/App.vue
+++ my-site/src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <Pager :total="total" :current="current" @page-change="handleChange" />
+    
+
 
   </div>
 </template>
 
 <script>
-import Pager from './components/Pager.vue';
+
+
 
 
 export default {
   name: 'App',
   components: {
-    Pager,
+   
   },
-  data() {
-    return {
-      total: 400,
-      current: 2
-    }
-  },
-  methods: {
-    handleChange(newPage) {
-      
-      this.current = newPage
-    }
-  }
+ 
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
