6f7bf6fbca7208714dd3d04bdc3421408e9afb82
Roseeeeeeeeeeeeeeeeee <165487175+Roseeeeeeeeeeeeeeeeee@users.noreply.github.com>
2025-04-25 00:14:14 +0800

    完成首页的主体部分

---
+++ my-site/src/views/Home/Carouselitem.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="carousel-item-container" >
+       <div>hfjakk</div>
+    </div>
+</template>
+
+<script>
+
+
+
+export default {
+   
+}
+</script>
+
+<style scoped lang="less">
+@import url('~@/styles/var.less');
+.carousel-item-container{
+    width: 100%;
+    height: 100%;
+    background-color: @dark;
+    color: #fff;
+}
+</style>
--- my-site/src/views/Home/index.vue
+++ my-site/src/views/Home/index.vue
@@ -1,24 +1,172 @@
 <template>
     <div class="home-container" ref="container">
-        主页
-       
+        <ul class="carousel-container" :style="{
+            marginTop,
+        }">
+            <li v-for="(item,i) in banners" :key="item.id">
+                <Carousel />
+            </li>
+        </ul>
+        <div class="arrow arrow-up" @click="moveTo(index - 1)"
+        v-show="index > 0">
+            <Icon type="arrowUp"/>
+        </div>
+        <div class="arrow arrow-down" @click="moveTo(index + 1)"
+        v-show="index < banners.length - 1">
+            <Icon type="arrowDown"/>
+
+        </div>
+        <div class="indicater">
+            <ul>
+                <li 
+                v-for="(item,i) in banners" 
+                :key="item.id" :class="{
+                    active:i === index
+                }"
+                @click="moveTo(i)"
+                >
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
 import getBanners from '../../api/banner';
-
-
+import Carousel from './Carouselitem.vue';
+import Icon from '@/components/Icon'
 export default {
-    async created(){
-       const data = await getBanners()
-       console.log(data);
-       
+    data() {
+        return {
+            banners: [],
+            index:0,
+            containerHeigh:0
+
+        }
     },
+    components :{
+        Carousel,
+        Icon
+    },
+
+    async created() {
+        const data = await getBanners()
+        this.banners = data;
+
+    },
+    mounted(){
+        this.containerHeigh = this.$refs.container.clientHeight;
+    },
+
     methods: {
-       
+        moveTo(i){
+            this.index = i;
+        }
+    },
+    computed:{
+        marginTop(){
+            return -this.index * this.containerHeigh + 'px';
+        }
     }
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+@import url('~@/styles/var.less');
+@import url('~@/styles/mixin.less');
+
+.home-container{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    // background-color: @dark;
+    position: relative;
+    color: #fff;
+    ul{
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+    .carousel-container{
+        width: 100%;
+        height: 100%;
+        transition: 2s;
+        li{
+            width: 100%;
+            height: 100%;
+        }
+    }
+    .arrow{
+        .self-center();
+       
+        transform: translateX(-50%);
+        font-size: 30px;
+        @gap:20px;
+        color: @gray;
+        &:hover{
+            cursor: pointer;
+            color: #fff;
+        }
+        &-up{
+            top:@gap;
+            animation: jump-up 2s infinite;
+        }
+        &-down{
+            bottom: @gap;
+            top: auto;
+            animation: jump-down 2s infinite;
+
+        }
+        @jump : 5px;
+        @keyframes jump-up{
+            0%{
+                transform: translate(-50%,-@jump);
+               
+            }
+            50%{
+                transform: translate(-50%,@jump);
+
+            }
+            100%{
+                transform: translate(-50%,-@jump);
+                
+
+            }
+        }
+        @keyframes jump-down{
+            0%{
+                transform: translate(-50%,@jump);
+
+            }
+            50%{
+                transform: translate(-50%,-@jump);
+
+            }
+            100%{
+                transform: translate(-50%,@jump);
+
+            }
+        }
+    }
+    .indicater{
+        .self-center();
+        right: 20px;
+        left: auto;
+
+        li{
+            width: 7px;
+            height: 7px;
+            border-radius: 50%;
+            border: 1px solid #fff;    
+            margin-bottom: 15px;
+            
+            &.active,&:hover{
+                background-color: #fff;
+                cursor: pointer;
+            }
+        }
+        
+
+    }
+}
+</style>
