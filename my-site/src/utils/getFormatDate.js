b603a5857e5ba484c7f9ea4cdfd97e9e2199f3ed
Roseeeeeeeeeeeeeeeeee <165487175+Roseeeeeeeeeeeeeeeeee@users.noreply.github.com>
2025-05-18 17:06:04 +0800

    修复一个错误

--- my-site/src/utils/getFormatDate.js
+++ my-site/src/utils/getFormatDate.js
@@ -10,7 +10,7 @@ export default function (timestamp, isDetail = false) {
         const hour = date.getHours().toString().padStart(2,'0');
         const min = date.getMinutes().toString().padStart(2,'0');
         const sec = date.getSeconds().toString().padStart(2,'0');
-        formatDate = formatDate + ' ' + ':'+hour + ':' + min + ":" + sec;
+        formatDate = formatDate + ' ' + ' '+hour + ':' + min + ":" + sec;
     }
     return formatDate;
 
